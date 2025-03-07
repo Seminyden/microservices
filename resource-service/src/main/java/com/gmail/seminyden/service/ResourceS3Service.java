983c91e5943f8984d7ab6d28c147511ebc44b25f
Dzianis_Semianitski <Dzianis_Semianitski@epam.com>
2025-03-07 15:04:47 +0400

    [module-1] Added resource-processor

---
+++ resource-service/src/main/java/com/gmail/seminyden/service/ResourceS3Service.java
@@ -0,0 +1,56 @@
+package com.gmail.seminyden.service;
+
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+
+@Service
+public class ResourceS3Service {
+
+    @Resource
+    private S3Client s3Client;
+
+    public void put(String bucket, String key, byte[] resource) {
+        s3Client.putObject(
+                PutObjectRequest.builder()
+                        .bucket(bucket)
+                        .key(key)
+                        .build(),
+                RequestBody.fromBytes(resource)
+        );
+    }
+
+    public byte[] get(String bucket, String key) {
+        try(
+                ResponseInputStream<GetObjectResponse> response =
+                        s3Client.getObject(
+                                GetObjectRequest.builder()
+                                        .bucket(bucket)
+                                        .key(key)
+                                        .build()
+                        )
+        ) {
+            return response.readAllBytes();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void delete(String bucket, String key) {
+        s3Client.deleteObject(
+                DeleteObjectRequest.builder()
+                        .bucket(bucket)
+                        .key(key)
+                        .build()
+        );
+    }
+}
